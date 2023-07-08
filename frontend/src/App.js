3628c5313d4fbf211e6be222fccaa0307e83cf57
banda-syam <syam@sixpep.com>
2023-07-08 14:09:43 +0530

    necessary updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import AuthenticatePage from "./components/AuthenticatePages/AuthenticatePage";
+import HomePage from "./components/HomePages/HomePage";
 
 function App() {
+  // let ip = "https://api.user.server.cryptocricket.io";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <>
+          <Route path="/authenticate" element={<AuthenticatePage />} />
+          <Route path="/" element={<HomePage />} />
+        </>
+      </Routes>
+    </>
   );
 }
 
